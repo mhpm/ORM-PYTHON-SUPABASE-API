0f1d1c52619a16ab403c9818d4cd440d72bb8263
Michelle Perez Morales <michelle.perez@sngular.com>
2024-11-20 13:45:17 -0600

    ➕ - Flask CORS Added

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes.users import users_bp
 
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app)
 
     # Register Blueprints
     app.register_blueprint(users_bp)
