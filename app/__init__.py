1eab70c1c4e7bc6002d0892e01f074610e70d368
Michelle Perez Morales <michelle.perez@sngular.com>
2024-11-20 20:48:14 -0600

    💚 - fix cors origin from vercel

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from app.routes.users import users_bp
 def create_app():
     app = Flask(__name__)
 
-    CORS(app)
+    CORS(app, resources={r"/*": {"origins": ["https://driscolls-eight.vercel.app"]}})
 
     # Register Blueprints
     app.register_blueprint(users_bp)
